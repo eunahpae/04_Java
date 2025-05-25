a6d04c5e8538335441eb8fae799c25891006b986
eunahPae <eunahp86@gmail.com>
2025-05-26 02:00:23 +0900

    주석 수정

--- chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application8.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application8.java
@@ -2,21 +2,21 @@ package com.ohgiraffers.section01.method;
 
 public class Application8 {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        /* comment. static 메소드를 호출할 수 있다. */
-        /* static 메소드를 호출하는 방법
-        *  클래스명.메소드명();
-        * */
+    /* comment. static 메소드를 호출할 수 있다. */
+    /* static 메소드를 호출하는 방법
+     *  클래스명.메소드명();
+     * */
 
-        Application8.sumTwoNumbers(10, 20);
+    Application8.sumTwoNumbers(10, 20);
 
-        sumTwoNumbers(20, 30);  // 동일한 클래스일 때 클래스명 생략 가능
+    sumTwoNumbers(20, 30);  // 동일한 클래스일 때 클래스명 생략 가능
 
-    }
+  }
 
-    public static int sumTwoNumbers(int first, int second) {
-
-        return first + second;
-    }
+  // static 이 붙으면 static 메소드, 붙지 않으면 Non-static 메소드라 부른다.
+  public static int sumTwoNumbers(int first, int second) {
+    return first + second;
+  }
 }
--- chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Calculator.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Calculator.java
@@ -2,13 +2,11 @@ package com.ohgiraffers.section01.method;
 
 public class Calculator {
 
-    public int minNumberOf(int first, int second) {
+  public int minNumberOf(int first, int second) {
+    return (first > second) ? second : first;
+  }
 
-        return (first > second)? second : first;
-    }
-
-    public static int maxNumberOf(int first, int second) {
-
-        return (first > second)? first : second;
-    }
+  public static int maxNumberOf(int first, int second) {
+    return (first > second) ? first : second;
+  }
 }
