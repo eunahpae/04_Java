6311754fe8442700d0f2081cdc472aa05760d68a
eunahPae <eunahp86@gmail.com>
2025-05-27 10:52:35 +0900

    continue 주석 수정

--- chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section03/branching/Application.java
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section03/branching/Application.java
@@ -2,13 +2,13 @@ package com.ohgiraffers.section03.branching;
 
 public class Application {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        A_break a = new A_break();
-//        a.testSimpleBreakStatement();
-        //a.testSimpleBreakStatement2();
+    A_break a = new A_break();
+    //a.testSimpleBreakStatement();
+    //a.testSimpleBreakStatement2();
 
-        B_continue b = new B_continue();
-        b.testSimpleContinueStatement();
-    }
+    B_continue b = new B_continue();
+    b.testSimpleContinueStatement();
+  }
 }
